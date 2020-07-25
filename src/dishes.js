f171fe3a6312892afacaa5c4c7d962e4e2e6c393
Abraham Sesay <abrahamsesay96@gmail.com>
2020-07-25 07:50:38 -0500

    build ui on top of bootstrap

---
+++ src/dishes.js
@@ -0,0 +1,39 @@
+const dishes = [
+    {
+        image:"https://images.unsplash.com/photo-1595357081381-c34f5634adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80",
+        dish: "Fried Egg Sandwich",
+        ingrids: "Egg, Sandwich",
+        price: "$268",
+        id: "ID:67384917"
+    },
+    {
+        image:"https://images.unsplash.com/photo-1594964643411-aacab76a7c0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        dish: "French Crostini",
+        ingrids: "French Spicy",
+        price: "$488",
+        id: "ID:1389313" 
+    },
+    {
+        image:"https://images.unsplash.com/photo-1595243766733-d2d4c7e3448e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1563&q=80",
+        dish: "Multigrain Hot Cereal",
+        ingrids: "Hot Spicy",
+        price: "$325",
+        id: "ID:13789315"
+    },
+    {
+        image:"https://images.unsplash.com/photo-1588532272851-661585d451e6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1608&q=80",
+        dish: "Lemon Yogurt Parfait",
+        ingrids: "Juice, Cold",
+        price: "$215",
+        id: "ID:17384917"
+    },
+    {
+        image:"https://images.unsplash.com/photo-1570179793258-c255e9f0c3b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMjF9&auto=format&fit=crop&w=1651&q=80",
+        dish: "Branch Special",
+        ingrids: "Hot, Spicy",
+        price: "$295",
+        id: "ID:17675917"
+    }
+]
+
+export default dishes
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDom from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDom.render(
+  <div className="menuCard">
+    <main role="main" className="container">
+    <div className="my-3 p-3 bg-white rounded shadow-sm">
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <h6 className="border-bottom border-gray pb-2 mb-0">Top Menu</h6>
+
+  
+    <div className="media text-muted pt-3">
+      <svg className="bd-placeholder-img mr-2 rounded" width="45" height="45" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <strong className="item-name text-gray-dark">Fried Egg</strong>
+          <a href="#">$268</a>
+        </div>
+
+        <span className="d-block">Eggs, Sandwich</span>
+        
+      </div>
+    </div>
+   
+   
+  </div>
+</main>
+
+  </div>, document.getElementById("root"))
